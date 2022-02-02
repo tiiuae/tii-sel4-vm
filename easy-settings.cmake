ec5ead7dc283c7981c9a9db2433f937610b64fe7
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2022-02-02 13:08:03 +0200

    HYPR-350 Set up common build system
    Signed-off-by: Hannu Lyytinen <hannu.lyytinen@unikie.com>

---
+++ easy-settings.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright 2019, Data61, CSIRO (ABN 41 687 119 230)
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+# set(PLATFORM "tx1" CACHE STRING "Platform to use")
+set(RELEASE OFF CACHE BOOL "Performance optimized build")
+# set(CAMKES_VM_APP "vm_minimal" CACHE STRING "CAmkES VM application to build")
