eeec433123774e84eda564c834fda8dc01964873
Markku Ahvenjärvi <markkux@ssrc.tii.ae>
2022-05-24 09:08:37 +0300

    Add VM defines for QEMU based systems
    Own defines remove the need for hacking and flagging
    configurations/vm.h.
    
    Signed-off-by: Markku Ahvenjärvi <markkux@ssrc.tii.ae>

---
+++ apps/Arm/vm_qemu_virtio/configurations/vm-qemu.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#define VM_QEMU_INIT_DEF() \
+    control; \
+    uses FileServerInterface fs; \
+    DEF_TK1DEVICEFWD \
+    DEF_KERNELARMPLATFORM_EXYNOS5410 \
+    maybe consumes restart restart_event; \
+    has semaphore vm_sem; \
+    consumes HaveNotification notification_ready; \
+    emits HaveNotification notification_ready_connector; \
+    maybe uses VMDTBPassthrough dtb_self; \
+    provides VMDTBPassthrough dtb; \
+    attribute int base_prio; \
+    attribute int num_vcpus = 1; \
+    attribute int num_extra_frame_caps; \
+    attribute int extra_frame_map_address; \
+    attribute { \
+        string linux_ram_base; \
+        string linux_ram_paddr_base; \
+        string linux_ram_size; \
+        string linux_ram_offset; \
+        string dtb_addr; \
+        string initrd_max_size; \
+        string initrd_addr; \
+    } linux_address_config; \
+    attribute { \
+        string linux_name = "linux"; \
+        string dtb_name = "linux-dtb"; \
+        string initrd_name = "linux-initrd"; \
+        string linux_bootcmdline = ""; \
+        string linux_stdout = ""; \
+    } linux_image_config; \
+
+#define VM_QEMU_CONFIGURATION_DEF(num) \
+    vm##num.fs_shmem_size = 0x100000; \
+    vm##num.global_endpoint_base = 1 << 27; \
+    vm##num.asid_pool = true; \
+    vm##num.simple = true; \
+    vm##num.sem_value = 0; \
+    vm##num.heap_size = 0x300000;
+
