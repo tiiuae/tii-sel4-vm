f7bb1e9fea00505d10e1b1f1496f9ea15c97ff73
Joonas Onatsu <joonas.onatsu@unikie.com>
2022-07-13 20:13:22 +0300

    test: Add single VM test apps
    Signed-off-by: Joonas Onatsu <joonasx@ssrc.tii.ae>

---
+++ apps/Arm/vm_single_test_hvc/settings.cmake
@@ -0,0 +1,20 @@
+#
+# Copyright 2018, Data61, CSIRO (ABN 41 687 119 230)
+# Copyright 2022, Technology Innovation Institute
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+set(supported "rpi4")
+if(NOT "${PLATFORM}" IN_LIST supported)
+    message(FATAL_ERROR "PLATFORM: ${PLATFORM} not supported.
+         Supported: ${supported}")
+endif()
+
+set(LibUSB OFF CACHE BOOL "" FORCE)
+set(VmPCISupport ON CACHE BOOL "" FORCE)
+set(VmVirtioConsole ON CACHE BOOL "" FORCE)
+set(VmVirtioNetArping OFF CACHE BOOL "" FORCE)
+set(VmVirtioNetVirtqueue OFF CACHE BOOL "" FORCE)
+set(VmInitRdFile ON CACHE BOOL "" FORCE)
+set(VmDtbFile OFF CACHE BOOL "" FORCE)
