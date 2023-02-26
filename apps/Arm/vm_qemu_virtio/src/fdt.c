6d80357d23e586de3da2245e06e3e2abd41096fc
Hannu Lyytinen <hannux@ssrc.tii.ae>
2023-02-27 01:04:37 +0200

    Move sources to tii-sel4-vm

---
+++ apps/Arm/vm_qemu_virtio/src/fdt.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2022, 2023, Technology Innovation Institute
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define ZF_LOG_LEVEL ZF_LOG_INFO
+
+#include <autoconf.h>
+#include <vm_qemu_virtio/gen_config.h>
+
+#include <sel4vm/guest_vm.h>
+
+#include <libfdt.h>
+#include <utils/util.h>
+
+int fdt_customize(vm_t *vm, void *gen_fdt)
+{
+    return 0;
+}
