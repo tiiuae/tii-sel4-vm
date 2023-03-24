5890f29b9bece10fe80c3556f68375e995a75f2a
Hannu Lyytinen <hannux@ssrc.tii.ae>
2023-03-24 20:30:10 +0200

    Create standalone trace module
    Signed-off-by: Hannu Lyytinen <hannux@ssrc.tii.ae>

--- apps/Arm/vm_qemu_virtio/src/trace.h
+++ apps/Arm/vm_qemu_virtio/src/trace.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022, Technology Innovation Institute
+ * Copyright 2022, 2023, Technology Innovation Institute
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -20,7 +20,6 @@
 
 /* guaranteed to always succeed, index always >= 1 */
 unsigned int trace_index(const char *);
-void trace_init(vm_t *vm);
 void trace_start(void);
 void trace_stop(void);
 void trace_dump(void);
@@ -29,7 +28,6 @@ void trace_dump(void);
 
 #define trace(__str__)
 static inline unsigned int trace_index(const char *str) { ARG_UNUSED(str); }
-static inline void trace_init(vm_t *vm) { ARG_UNUSED(vm); ZF_LOGI("seL4 tracing is not supported by configuration"); }
 static inline void trace_start(void) {}
 static inline void trace_stop(void) {}
 static inline void trace_dump(void) {}
