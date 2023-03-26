35bf8df31cf6572b12fd8910f7347448ea7d9b0f
Ivan Kuznetsov <jsvapiav@gmail.com>
2023-03-26 22:38:14 +0300

    rpi4: dtb: Create USB PCIe subnode
    camkes-tool's support for multiple levels is still very poor. This
    workaround adds the "usb@0,0" at run time instead.
    
    jira: HYPR-48
    Signed-off-by: Ivan Kuznetsov <jsvapiav@gmail.com>
    
    Hot-Fix: user-vm boot hangs
    
    * Don't try to add usb@1,0 node to user-vm DTS
    
    Signed-off-by: Ivan Kuznetsov <jsvapiav@gmail.com>
    
    Flag USB node generation
    
    Fixes boot error for images without USB pass-through.
    
    Signed-off-by: Markku Ahvenjärvi <markkux@ssrc.tii.ae>
    
    vm-arm: Use vmid attribute to identify VMs
    
    Signed-off-by: Joonas Onatsu <joonasx@ssrc.tii.ae>
    
    Fix undefined reference to vmid
    
    Fix for failing non-QEMU builds.
    
    Signed-off-by: Markku Ahvenjärvi <markkux@ssrc.tii.ae>
    
    trivial: Fix style
    
    Signed-off-by: Ivan Kuznetsov <jsvapiav@gmail.com>

--- src/plat/rpi4/fdt.c
+++ src/plat/rpi4/fdt.c
@@ -8,6 +8,66 @@
 
 #include <fdt_custom.h>
 
+int fdt_generate_usb_node(void *fdt)
+{
+    int root_offset = fdt_path_offset(fdt, "/scb/pcie@7d500000/pci@0,0");
+    int address_cells = fdt_address_cells(fdt, root_offset);
+    int size_cells = fdt_size_cells(fdt, root_offset);
+
+    if (root_offset <= 0) {
+        ZF_LOGE("root_offset: %d", root_offset);
+    }
+
+    int this = fdt_add_subnode(fdt, root_offset, "usb@0,0");
+    if (this < 0) {
+        ZF_LOGE("Can't add usb@1,0 subnode: %d", this);
+        return this;
+    }
+
+    int err = fdt_appendprop_u32(fdt, this, "resets", 0x2d);
+    if (err) {
+        ZF_LOGE("Can't append resets property: %d", this);
+        return err;
+    }
+    err = fdt_appendprop_u32(fdt, this, "resets", 0x00);
+    if (err) {
+        ZF_LOGE("Can't append resets property: %d", this);
+        return err;
+    }
+
+    err = fdt_appendprop_u32(fdt, this, "reg", 0x10000);
+    if (err) {
+        ZF_LOGE("Can't append reg property: %d", this);
+        return err;
+    }
+
+    err = fdt_appendprop_u32(fdt, this, "reg", 0x00);
+    if (err) {
+        ZF_LOGE("Can't append reg property: %d", this);
+        return err;
+    }
+
+    err = fdt_appendprop_u32(fdt, this, "reg", 0x00);
+    if (err) {
+        ZF_LOGE("Can't append reg property: %d", this);
+        return err;
+    }
+
+    err = fdt_appendprop_u32(fdt, this, "reg", 0x00);
+    if (err) {
+        ZF_LOGE("Can't append reg property: %d", this);
+        return err;
+    }
+
+    err = fdt_appendprop_u32(fdt, this, "reg", 0x00);
+    if (err) {
+        ZF_LOGE("Can't append reg property: %d", this);
+        return err;
+    }
+
+    return 0;
+}
+
 int fdt_plat_customize(vm_t *vm, void *gen_fdt)
 {
     int err;
