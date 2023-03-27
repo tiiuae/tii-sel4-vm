b2273cc4fb6005809dcd44c8d2069caac5a80c0e
Ivan Kuznetsov <jsvapiav@gmail.com>
2023-03-27 12:18:41 +0300

    trace: dtb: Create nodes
    * Add ramoops node with ftrace caps -- disabled
    * Add sel4_tracebuffer node to AbstractVM
    
    jira: HYPR-432
    Signed-off-by: Ivan Kuznetsov <jsvapiav@gmail.com>

--- src/fdt.c
+++ src/fdt.c
@@ -8,11 +8,124 @@
 
 #include <fdt_custom.h>
 
+extern const int __attribute__((weak)) tracebuffer_base;
+extern const int __attribute__((weak)) tracebuffer_size;
+extern const int __attribute__((weak)) ramoops_base;
+extern const int __attribute__((weak)) ramoops_size;
+
+int fdt_generate_sel4_tracebuffer_node(void *fdt, unsigned long base, size_t size)
+{
+    int root_offset = fdt_path_offset(fdt, "/reserved-memory");
+    int address_cells = fdt_address_cells(fdt, root_offset);
+    int size_cells = fdt_size_cells(fdt, root_offset);
+
+    ZF_LOGE("/reserved-memory node: 0x%x", root_offset);
+
+    int this = fdt_add_subnode(fdt, root_offset, "sel4_tracebuffer");
+    if (this < 0) {
+        ZF_LOGE("Can not create sel4_tracebuffer node");
+        return this;
+    }
+
+    int err = fdt_appendprop_string(fdt, this, "compatible", "sel4_tracebuffer");
+    if (err) {
+        ZF_LOGE("Can not set compatible prop");
+        return err;
+    }
+
+    err = append_prop_with_cells(fdt, this, base, address_cells, "reg");
+    if (err) {
+        ZF_LOGE("Can not add reg prop");
+        return err;
+    }
+
+    err = append_prop_with_cells(fdt, this, size, size_cells, "reg");
+    if (err) {
+        ZF_LOGE("Can not add reg2 prop");
+        return err;
+    }
+
+    return 0;
+}
+
+int fdt_generate_ramoops_node(void *fdt, unsigned long base, size_t size)
+{
+    int root_offset = fdt_path_offset(fdt, "/reserved-memory");
+    int address_cells = fdt_address_cells(fdt, root_offset);
+    int size_cells = fdt_size_cells(fdt, root_offset);
+
+    ZF_LOGE("/reserved-memory node: 0x%x", root_offset);
+
+    int this = fdt_add_subnode(fdt, root_offset, "ramoops");
+    if (this < 0) {
+        ZF_LOGE("Can not create ramoops node");
+        return this;
+    }
+
+    int err = fdt_appendprop_string(fdt, this, "compatible", "ramoops");
+    if (err) {
+        ZF_LOGE("Can not set compatible prop");
+        return err;
+    }
+
+    err = append_prop_with_cells(fdt, this, base, address_cells, "reg");
+    if (err) {
+        ZF_LOGE("Can not add reg prop");
+        return err;
+    }
+
+    err = append_prop_with_cells(fdt, this, size, size_cells, "reg");
+    if (err) {
+        ZF_LOGE("Can not add reg2 prop");
+        return err;
+    }
+
+    err = append_prop_with_cells(fdt, this, size, 1, "ftrace-size");
+    if (err) {
+        ZF_LOGE("Can not add ftrace-size");
+        return err;
+    }
+
+    return 0;
+}
+
+static int fdt_generate_trace_nodes(void *gen_fdt)
+{
+    int err;
+
+    if (vmid == 0) {
+        if (&tracebuffer_base && &tracebuffer_size && tracebuffer_base && tracebuffer_size) {
+            ZF_LOGE("Trying to add sel4 tracebuffer node: 0x%lx@0x%lx", tracebuffer_size, tracebuffer_base);
+
+            err = fdt_generate_sel4_tracebuffer_node(gen_fdt, tracebuffer_base, tracebuffer_size);
+            if (err) {
+                return -1;
+            }
+        }
+
+        if (&ramoops_base && &ramoops_size && ramoops_base && ramoops_size) {
+            ZF_LOGE("Trying to add ramoops node: 0x%lx@0x%lx", ramoops_size, ramoops_base);
+
+            err = fdt_generate_ramoops_node(gen_fdt, ramoops_base, ramoops_size);
+            if (err) {
+                return -1;
+            }
+        }
+    }
+
+    return 0;
+}
+
 int fdt_customize(vm_t *vm, void *gen_fdt)
 {
     int err;
 
     err = fdt_plat_customize(vm, gen_fdt);
+    if (err) {
+        return err;
+    }
+
+    err = fdt_generate_trace_nodes(gen_fdt);
     if (err) {
         return err;
     }
