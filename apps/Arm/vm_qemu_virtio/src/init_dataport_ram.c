6400a0d731b7bb764f822e4889bf5c80118c9d2e
Hannu Lyytinen <hannux@ssrc.tii.ae>
2023-05-08 10:51:15 +0300

    Align to upstream changes
    Signed-off-by: Hannu Lyytinen <hannux@ssrc.tii.ae>

--- apps/Arm/vm_qemu_virtio/src/init_dataport_ram.c
+++ apps/Arm/vm_qemu_virtio/src/init_dataport_ram.c
@@ -18,23 +18,36 @@ extern const int vmid;
 
 extern dataport_caps_handle_t memdev_handle;
 
+/* Hack to pass the information to vm_ram_touch() in libsel4vm/src/guest_ram.c
+ * without making libsel4vm depend on CAmkES being linked in.
+ */
+uintptr_t __attribute__((weak)) ram_base(void)
+{
+    return vm_config.ram.base;
+}
+
+size_t __attribute__((weak)) ram_size(void)
+{
+    return vm_config.ram.size;
+}
+
 static vm_frame_t dataport_memory_iterator(uintptr_t addr, void *cookie)
 {
     cspacepath_t return_frame;
     vm_frame_t frame_result = { seL4_CapNull, seL4_NoRights, 0, 0 };
 
     int sz = seL4_PageBits;
-    if (addr >= ram_base && addr < (ram_base + ram_size)) {
+    if (addr >= vm_config.ram.base && addr < (vm_config.ram.base + vm_config.ram.size)) {
         sz = seL4_LargePageBits;
     }
 
     uintptr_t frame_start = ROUND_DOWN(addr, BIT(sz));
-    if (frame_start < ram_base || frame_start > ram_base + ram_size) {
+    if (frame_start < vm_config.ram.base || frame_start > vm_config.ram.base + vm_config.ram.size) {
         ZF_LOGE("Error: Not dataport ram region");
         return frame_result;
     }
 
-    int page_idx = (frame_start - ram_base) / BIT(sz);
+    int page_idx = (frame_start - vm_config.ram.base) / BIT(sz);
     frame_result.cptr = dataport_get_nth_frame_cap(&memdev_handle, page_idx);
     frame_result.rights = seL4_AllRights;
     frame_result.vaddr = frame_start;
@@ -44,7 +57,7 @@ static vm_frame_t dataport_memory_iterator(uintptr_t addr, void *cookie)
 
 static void original_init_ram_module(vm_t *vm, void *cookie)
 {
-    int err = vm_ram_register_at(vm, ram_base, ram_size, vm->mem.map_one_to_one);
+    int err = vm_ram_register_at(vm, vm_config.ram.base, vm_config.ram.size, vm_config.map_one_to_one);
     assert(!err);
 }
 
@@ -60,6 +73,6 @@ void init_ram_module(vm_t *vm, void *cookie)
 
     ZF_LOGI("initializing RAM module from dataport");
 
-    err = vm_ram_register_at_custom_iterator(vm, ram_base, ram_size, dataport_memory_iterator, NULL);
+    err = vm_ram_register_at_custom_iterator(vm, vm_config.ram.base, vm_config.ram.size, dataport_memory_iterator, NULL);
     assert(!err);
 }
