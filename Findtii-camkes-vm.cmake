2f83a33bfc47f26b7630aead11dbb44d3bbbdb57
Hannu Lyytinen <hannux@ssrc.tii.ae>
2023-09-03 10:32:17 +0300

    Refactor build system
    Signed-off-by: Hannu Lyytinen <hannux@ssrc.tii.ae>

--- Findtii-camkes-vm.cmake
+++ Findtii-camkes-vm.cmake
@@ -5,7 +5,17 @@
 #
 
 set(TII_CAMKES_VM_DIR "${CMAKE_CURRENT_LIST_DIR}" CACHE STRING "")
-mark_as_advanced(TII_CAMKES_VM_DIR)
+set(TII_CAMKES_VM_HELPERS_PATH "${CMAKE_CURRENT_LIST_DIR}/tii_camkes_vm_helpers.cmake" CACHE STRING "")
+mark_as_advanced(TII_CAMKES_VM_DIR TII_CAMKES_VM_HELPERS_PATH)
+
+macro(tii_camkes_vm_setup)
+if(AppArch STREQUAL "Arm")
+    find_package(camkes-arm-vm REQUIRED)
+    camkes_arm_vm_setup_arm_vm_environment()
+else()
+    message(FATAL_ERROR "Unsupported")
+endif()
+endmacro()
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(tii-camkes-vm DEFAULT_MSG TII_CAMKES_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(tii-camkes-vm DEFAULT_MSG TII_CAMKES_VM_DIR TII_CAMKES_VM_HELPERS_PATH)
