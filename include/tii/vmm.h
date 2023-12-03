92d54aec2b9f50fb6ace54ceb8eabd12bdd2c875
Hannu Lyytinen <hannux@ssrc.tii.ae>
2023-12-03 13:42:22 +0200

    Add VMM interface
    Hide CAmkES-VM specific details.
    
    Signed-off-by: Hannu Lyytinen <hannux@ssrc.tii.ae>

---
+++ include/tii/vmm.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2023, Technology Innovation Institute
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+int vmm_module_init_by_name(const char *name, void *cookie);
